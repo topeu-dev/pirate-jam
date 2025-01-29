8fca4f6a3265b2e554a0b5b61fd6f530dce7c1ee
Festus2345 <166950201+Festus2345@users.noreply.github.com>
2025-01-29 05:45:04 +0300

    LoseWinAdded

---
+++ Assets/DanyMess/LoseSceneEnd.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoseSceneEnd : MonoBehaviour
+{
+    
+    // Публичная переменная для индекса сцены
+    public int sceneIndexRestart = 1;
+    public int sceneIndexMenu = 0;
+
+    
+
+  
+
+    // Метод, который запускается при клике на кнопку
+    public void RestartLoad()
+    {
+
+        SceneManager.LoadScene(sceneIndexRestart);
+    }
+    public void MenuLoad()
+    {
+
+        SceneManager.LoadScene(sceneIndexMenu);
+    }
+}
