7fbb125b1b7288d878977d0a77fa498bcb7f0839
topeu <urih.daniil@gmail.com>
2025-01-31 02:48:32 +0100

    build #1 T_T

--- Assets/Scripts/Game/GameState.cs
+++ Assets/Scripts/Game/GameState.cs
@@ -55,7 +55,7 @@ namespace Game
 
         private void ToggleFovState(InputAction.CallbackContext obj)
         {
-            sceneState.isFovEnabled = !sceneState;
+            sceneState.isFovEnabled = !sceneState.isFovEnabled;
         }
 
         private void OnDisable()
